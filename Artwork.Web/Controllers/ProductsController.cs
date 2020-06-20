463440712b21a8cf0e6b5d002d5fa191e9eae732
Nana Essandoh <nanaessandoh@gmail.com>
2020-06-19 22:48:24 -0230

    Add project files.

---
+++ Artwork.Web/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Artwork.Web.Models;
+using Artwork.Web.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Artwork.Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService; 
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+        //[HttpPatch]
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery]string ProductId,[FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+       
+    }
+}
